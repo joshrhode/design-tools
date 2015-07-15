bda1838d70c401332e88694d423598d1c7992ac1
josh <josh@tinypass.com>
2015-07-15 14:54:38 -0400

    Colorguard — Photoshop CC
    Brand new features!
    
    1. Target a single PSD, or a folder of PSDs. If you close all of your
    PSDs and run the script, you will be asked to select a folder — then the
    script will update (but not save) all of the PSDs inside of it. If you
    have a PSD open, the script will only target that PSD.
    
    2. The script now targets text layers too! It will replace colors in
    your text just like your vector shapes.
    
    3. Optimized a bunch of code so it's even faster.

--- Colorguard.jsx
+++ Colorguard.jsx
@@ -7,24 +7,40 @@
     ———
     
     Add a list of colors you want to find and replace.
-    This script will open every PSD in a folder and then
-    automatically update the fill color of every vector/shape
-    inside of it.
+    This script will automatically update the fill colors
+    of every vector/shape and text layer in your PSDs.
 
     Why? I hacked this script together to help with a large
     rebranding project. We have hundreds of PSDs and needed
     a fast way to update the color palette across
     all of them instantly.
 
-    1.  Inside of this script, add the list of colors you want
+
+	HOW IT WORKS
+    ———
+	
+	If you have an active PSD open, this script
+	will only target your active PSD.
+
+	If you close all of your documents, the script
+	will let you select a folder, then it will
+	automatically open and update every PSD inside of it.
+
+	It won't save the files - that's up to you :)
+
+
+	HOW TO USE IT
+    ———
+	
+    1.  Down in this script, edit the list of colors you want
     	to find and replace inside of the 'colorTable' array. 
     	
     	Example: Your client's brand color used to be blue.
-    	Now it's red. You have 250 PSDs. Hoo boy.
-	
+    	They want it to be red. You have 250 PSDs. No problem:
 		[{find:'0000ff', replace:'ff000'}];
 
-    2.  Run this script, select a folder of PSDs, and go!
+    2.  Run this script!
+
 
     INSTALL
     ———
@@ -48,25 +64,54 @@ var numProcessed = 0;
 
 function main() {  
 
+	setupColorObjects();
+
 	 if (app.documents.length > 0) {
-        alert ('Close all open documents before running this script.');
-        return;
-    }
+        	    
+	    doc = app.activeDocument;
+	    activeLayer = doc.activeLayer;
+
+	    colorizeLayers();
+	    
+	    alert('Total shape colors updated: ' + numProcessed);
+
+    } else if (app.documents.length == 0) {
     
-    var sourceFolder = Folder.selectDialog ('Select the folder of PSDs you want to update.', Folder.myDocuments);
-    var files = sourceFolder.getFiles('*.psd');
-       
-    for (var i = 0; i < files.length; i++) {
-        var f = files[i];
-        if (f instanceof Folder)
-            continue;
+	    var sourceFolder = Folder.selectDialog ('Select the folder of PSDs you want to update.', Folder.myDocuments);
+	    	if(sourceFolder == null) return;
 
-        doc = app.open (f);
-        colorizeLayers();
-    }
+	    var files = sourceFolder.getFiles('*.psd');
+	    	if(files == null || files.length == 0) return;
+		
+	    for (var i = 0; i < files.length; i++) {
+	        var f = files[i];
+	        if (f instanceof Folder)
+	            continue;
 
-    alert('Total shape colors updated: ' + numProcessed + ' in ' + files.length + ' files');
+	        doc = app.open (f);
+	        colorizeLayers();
+	    }
+
+	    alert('Total shape colors updated: ' + numProcessed + ' in ' + files.length + ' files');
+
+	}
 	
+}
+
+function setupColorObjects() {
+	for(var i = 0; i < colorTable.length; ++i) {
+		
+		var findColor = new SolidColor,
+			replaceColor = new SolidColor;
+		
+		findColor.rgb.hexValue = colorTable[i].find.toUpperCase();  
+		colorTable[i].find = findColor;
+
+		replaceColor.rgb.hexValue = colorTable[i].replace.toUpperCase();  
+		colorTable[i].replace = replaceColor;
+
+
+	}
 }
 
 function colorizeLayers() {
@@ -74,8 +119,8 @@ function colorizeLayers() {
 	// get number of layers;  
 	var ref = new ActionReference();  
 	ref.putEnumerated( charIDToTypeID('Dcmn'), charIDToTypeID('Ordn'), charIDToTypeID('Trgt') );  
-	var applicationDesc = executeActionGet(ref);  
-	var totalLayers = applicationDesc.getInteger(stringIDToTypeID('numberOfLayers'));  
+	var descApplication = executeActionGet(ref);  
+	var totalLayers = descApplication.getInteger(stringIDToTypeID('numberOfLayers'));  
 
 	//start a progress window
 	progressWindow = createProgressWindow('Coloring...');
@@ -87,12 +132,12 @@ function colorizeLayers() {
 		try {  
 			var ref = new ActionReference();  
 			ref.putIndex( charIDToTypeID('Lyr '), i);
-			var layerDesc = executeActionGet(ref);  
+			var descLayer = executeActionGet(ref);  
 
-			var layerKind = layerDesc.getString(stringIDToTypeID('layerKind'));  
+			var layerKind = descLayer.getString(stringIDToTypeID('layerKind'));  
 
-			if(layerKind == 4)
-				numProcessed += inspectLayer(ref, i);
+			if(layerKind == 3 || layerKind == 4)
+				numProcessed += inspectLayer(layerKind, descLayer, i);
 			  
 			progressWindow.bar.value = (100 / (progressStep / i));
 
@@ -104,24 +149,78 @@ function colorizeLayers() {
 }
 
 
-
-function inspectLayer(ref, refIndex) {
-	var currentColor = getFillColor(ref).rgb.hexValue;
-	for(var i = 0; i < colorTable.length; ++i) {
-		if(currentColor == colorTable[i].find.toUpperCase()) {
-			setFillColor(refIndex, colorTable[i].replace);
-			return 1;
+//inspect the shape/text layer, compare its color, and if it's a match, update it
+function inspectLayer(kind, descLayer, refIndex) {
+	var currentColor;
+	//it's a shape layer!
+	if(kind == 4) {
+		currentColor = getFillColor(descLayer);
+		for(var i = 0; i < colorTable.length; ++i) {
+			if(currentColor.rgb.hexValue == colorTable[i].find.rgb.hexValue) {
+				setFillColor(refIndex, colorTable[i].replace);
+				return 1;
+			}
+		}
+	//it's a text layer!
+	} else if (kind == 3) {
+		currentColor = getTextColor(descLayer);
+		for(var i = 0; i < colorTable.length; ++i) {
+			if(currentColor.rgb.hexValue == colorTable[i].find.rgb.hexValue) {
+				setTextColor(refIndex, colorTable[i].replace);
+				return 1;
+			}
 		}
 	}
 	return 0;
 }
 
+//return a photoshop SolidColor object
+function parseColor(color) {
+	var colorObject = new SolidColor;
+	colorObject.rgb.red = color.getDouble(charIDToTypeID('Rd  '));
+	colorObject.rgb.green = color.getDouble(charIDToTypeID('Grn '));
+	colorObject.rgb.blue = color.getDouble(charIDToTypeID('Bl  '));
+	return colorObject;
+}
+
+
+function setTextColor(refIndex, newColor) {
+
+	var descLayer = new ActionDescriptor();  
+	var refLayer = new ActionReference();
+	refLayer.putProperty( charIDToTypeID( "Prpr" ), charIDToTypeID( "TxtS" ) );
+	refLayer.putIndex( charIDToTypeID( "TxLr" ), refIndex );
+	descLayer.putReference( charIDToTypeID('null'), refLayer );
+	
+	var textDesc = new ActionDescriptor();
+	textDesc.putInteger( stringIDToTypeID( "textOverrideFeatureName" ), 808466226 );
+	textDesc.putInteger( stringIDToTypeID( "typeStyleOperationType" ), 3 );
+	
+	var colorDesc = new ActionDescriptor();  
+	colorDesc.putDouble( charIDToTypeID('Rd  '), newColor.rgb.red );  
+	colorDesc.putDouble( charIDToTypeID('Grn '), newColor.rgb.green );  
+	colorDesc.putDouble( charIDToTypeID('Bl  '), newColor.rgb.blue );  
+
+	textDesc.putObject( charIDToTypeID( "Clr " ), charIDToTypeID( "RGBC" ), colorDesc );
+	descLayer.putObject( charIDToTypeID( "T   " ), charIDToTypeID( "TxtS" ), textDesc );
+	executeAction( charIDToTypeID( "setd" ), descLayer, DialogModes.NO );
+
+}
+
+function getTextColor(descLayer){
+	
+	var list =  descLayer.getObjectValue(charIDToTypeID("Txt ")) ;  
+	var tsr =  list.getList(charIDToTypeID("Txtt")) ;  
+	var tsr0 =  tsr.getObjectValue(0) ;  
+	var textStyle = tsr0.getObjectValue(charIDToTypeID("TxtS"));  
+	var color = textStyle.getObjectValue(charIDToTypeID('Clr '));   
+	
+	return parseColor(color);
+	
+}
  
 function setFillColor(refIndex, newColor) {
 	
-	var setColor =  new SolidColor;  
-	setColor.rgb.hexValue = newColor;  
-
 	var desc = new ActionDescriptor();  
 	var ref = new ActionReference();  
 	ref.putIndex(stringIDToTypeID('contentLayer'), refIndex);
@@ -129,26 +228,21 @@ function setFillColor(refIndex, newColor) {
 
 	var fillDesc = new ActionDescriptor();  
 	var colorDesc = new ActionDescriptor();  
-	colorDesc.putDouble( charIDToTypeID('Rd  '), setColor.rgb.red );  
-	colorDesc.putDouble( charIDToTypeID('Grn '), setColor.rgb.green );  
-	colorDesc.putDouble( charIDToTypeID('Bl  '), setColor.rgb.blue );  
+	colorDesc.putDouble( charIDToTypeID('Rd  '), newColor.rgb.red );  
+	colorDesc.putDouble( charIDToTypeID('Grn '), newColor.rgb.green );  
+	colorDesc.putDouble( charIDToTypeID('Bl  '), newColor.rgb.blue );  
 	fillDesc.putObject( charIDToTypeID('Clr '), charIDToTypeID('RGBC'), colorDesc );  
 	desc.putObject( charIDToTypeID('T   '), stringIDToTypeID('solidColorLayer'), fillDesc );  
 	executeAction( charIDToTypeID('setd'), desc, DialogModes.NO );  
 
 }
  
-function getFillColor(ref){
+function getFillColor(descLayer){
 	
-	var newRef = executeActionGet( ref );
-	var list =  newRef.getList( charIDToTypeID( "Adjs" ) ) ;
+	var list =  descLayer.getList( charIDToTypeID( "Adjs" ) ) ;
 	var solidColorLayer = list.getObjectValue(0);        
 	var color = solidColorLayer.getObjectValue(charIDToTypeID('Clr ')); 
-	var fillcolor = new SolidColor;
-	fillcolor.rgb.red = color.getDouble(charIDToTypeID('Rd  '));
-	fillcolor.rgb.green = color.getDouble(charIDToTypeID('Grn '));
-	fillcolor.rgb.blue = color.getDouble(charIDToTypeID('Bl  '));
-	return fillcolor;
+	return parseColor(color);
 
 }
 
